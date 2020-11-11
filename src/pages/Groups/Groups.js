b1e41f41196a826c1ec31eff730802f259f51b36
Soufian Elhanafi <soufelhanafi@gmail.com>
2020-11-11 09:05:05 +0100

    update pagination

--- src/pages/Groups/Groups.js
+++ src/pages/Groups/Groups.js
@@ -96,9 +96,6 @@ const Groups = () => {
                     </div>
                     <ul className="pagination clearfix">
                         <li><a href="/">1</a></li>
-                        <li><a href="/">2</a></li>
-                        <li><a href="/">3</a></li>
-                        <li><a href="/">4</a></li>
                     </ul>
                 </div>
             </section>
