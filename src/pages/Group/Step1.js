00fd748dedf575b17242bd51482a5b17ffb11592
Soufian Elhanafi <soufelhanafi@gmail.com>
2020-11-11 01:59:46 +0100

    update code and add list of groups

--- src/pages/Group/Step1.js
+++ src/pages/Group/Step1.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react'
 import { Redirect } from 'react-router-dom';
 import Backdrop from '@material-ui/core/Backdrop';
 import CircularProgress from '@material-ui/core/CircularProgress';
-import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 import axios from 'axios';
 
--- src/pages/Groups/Groups.js
+++ src/pages/Groups/Groups.js
@@ -1,14 +1,67 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import PageTitle from '../../components/PageTitle';
+import Backdrop from '@material-ui/core/Backdrop';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { makeStyles } from '@material-ui/core/styles';
+import axios from 'axios';
+
+const baseUrl = process.env.NODE_ENV === 'development'?"http://localhost:4000":"https://teachiate-backend.fnmotivations.com/"
+
+const useStyles = makeStyles((theme) => ({
+  backdrop: {
+    zIndex: theme.zIndex.drawer + 1,
+    color: '#fff',
+  },
+  text: {
+    top: "50px",
+    position: "relative",
+    left: "-50px"
+  },
+  cover:{
+    width: 73,
+    height: 73,
+    borderRadius: '50%'
+  }
+}));
+
 
 const Groups = () => {
-
+  const classes = useStyles();
+  const [groups, setGroups] = useState([])
+  const [currentPage, setCurrentPage] = useState([])
+  const [limit, setLimit] = useState([])
+  const [totalElement, setTotalElement] = useState([])
+  const [totalPages, setTotalPages] = useState([])
+  const [open, setOpen] = useState(false)
     useEffect(() => {
         window.scrollTo(0, 0);
-    }, []);
+        getGroups(currentPage, limit);
+    }, [currentPage, limit]);
+    const getGroups = async (page, limit) => {
+      setOpen(true)
+      axios({
+        url: `${baseUrl}/group/list`,
+        method: 'get'
+      }).then((response)=>{
+        debugger
+        const { data } = response.data
+        setGroups(data.groups)
+        setCurrentPage(data.page)
+        setLimit(limit)
+        setTotalElement(data.totalElement)
+        setTotalPages(Math.ceil(data.totalElement/limit))
+        setOpen(false)
+      }).catch((e)=>{
+        console.log(e);
+      })
+    }
 
     return (
         <>
+          <Backdrop className={classes.backdrop} open={open} >
+            <CircularProgress color="inherit" />
+            <div className={classes.text}>Loading</div>
+          </Backdrop>
             <PageTitle title='Groups'/>
             <section className="inner_content groups people_content">
                 <div className="container">
@@ -17,7 +70,7 @@ const Groups = () => {
                             <h3 className="main_sec_title">All Groups</h3>
                         </div>
                         <div className="col-md-3 col-sm-4">
-                            
+
                         </div>
                     </div>
                     <div className="row">
@@ -29,120 +82,17 @@ const Groups = () => {
                         </div>
                     </div>
                     <div className="row">
+                    {groups.map((group, index)=>{
+                      return(
                         <div className="col-md-3 col-sm-6 col-xs-12">
                             <div className="group_des text-center">
-                                <img src="assets/img/g1.png" alt=""/>
-                                <h4>Test group<span>created 2 weeks</span></h4>
-                                <div className="catagory"><a href="/">Public Group</a></div>
+                                <img className={classes.cover} src={group.avatar||'assets/img/g1.png'} alt=""/>
+                                <h4>{group.groupName}{/*<span>created 2 weeks</span>*/}</h4>
+                                <div className="catagory"><a href="/">{group.privacy === 'PUBLIC'? 'Public Group':'Private Group'}</a></div>
                             </div>
                         </div>
-                        <div className="col-md-3 col-sm-6 col-xs-12">
-                            <div className="group_des text-center">
-                                <img src="assets/img/g2.png" alt=""/>
-                                <h4>Homeschool Martial Arts<span>created 2 weeks</span></h4>
-                                <div className="catagory"><a href="/">Public Group</a></div>
-                            </div>
-                        </div>
-                        <div className="col-md-3 col-sm-6 col-xs-12">
-                            <div className="group_des text-center">
-                                <img src="assets/img/g3.png" alt=""/>
-                                <h4>Homeschooling gifted children<span>created 4 days ago</span></h4>
-                                <div className="catagory"><a href="/">Public Group</a></div>
-                            </div>
-                        </div>
-                        <div className="col-md-3 col-sm-6 col-xs-12">
-                            <div className="group_des text-center">
-                                <img src="assets/img/g4.png" alt=""/>
-                                <h4>Middle school students<span>created 4 days ago</span></h4>
-                                <div className="catagory"><a href="/">Public Group</a></div>
-                            </div>
-                        </div>
-                        <div className="col-md-3 col-sm-6 col-xs-12">
-                            <div className="group_des text-center">
-                                <img src="assets/img/g5.png" alt=""/>
-                                <h4>Homeschooling gifted children<span>created 2 weeks</span></h4>
-                                <div className="catagory"><a href="/">Public Group</a></div>
-
-                            </div>
-                        </div>
-                        <div className="col-md-3 col-sm-6 col-xs-12">
-                            <div className="group_des text-center">
-                                <img src="assets/img/g6.png" alt=""/>
-                                <h4>Homeschool Martial Arts<span>created 2 weeks</span></h4>
-                                <div className="catagory"><a href="/">Public Group</a></div>
-                            </div>
-                        </div>
-                        <div className="col-md-3 col-sm-6 col-xs-12">
-                            <div className="group_des text-center">
-                                <img src="assets/img/g3.png" alt=""/>
-                                <h4>Homeschooling gifted children<span>created 4 days ago</span></h4>
-                                <div className="catagory"><a href="/">Public Group</a></div>
-
-                            </div>
-                        </div>
-                        <div className="col-md-3 col-sm-6 col-xs-12">
-                            <div className="group_des text-center">
-                                <img src="assets/img/g7.png" alt=""/>
-                                <h4>Albert Brown<span>6 hours ago</span></h4>
-                                <div className="catagory"><a href="/">Public Group</a></div>
-                            </div>
-                        </div>
-                        <div className="col-md-3 col-sm-6 col-xs-12">
-                            <div className="group_des text-center">
-                                <img src="assets/img/g1.png" alt=""/>
-                                <h4>Test group<span>created 2 weeks</span></h4>
-                                <div className="catagory"><a href="/">Public Group</a></div>
-                            </div>
-                        </div>
-                        <div className="col-md-3 col-sm-6 col-xs-12">
-                            <div className="group_des text-center">
-                                <img src="assets/img/g2.png" alt=""/>
-                                <h4>Alexa Jennings<span>6 hours ago</span></h4>
-                                <div className="catagory"><a href="/">Public Group</a></div>
-                            </div>
-                        </div>
-                        <div className="col-md-3 col-sm-6 col-xs-12">
-                            <div className="group_des text-center">
-                                <img src="assets/img/g3.png" alt=""/>
-                                <h4>Homeschooling gifted children<span>created 4 days ago</span></h4>
-                                <div className="catagory"><a href="/">Public Group</a></div>
-                            </div>
-                        </div>
-                        <div className="col-md-3 col-sm-6 col-xs-12">
-                            <div className="group_des text-center">
-                                <img src="assets/img/g4.png" alt=""/>
-                                <h4>Middle school students<span>created 4 days ago</span></h4>
-                                <div className="catagory"><a href="/">Public Group</a></div>
-                            </div>
-                        </div>
-                        <div className="col-md-3 col-sm-6 col-xs-12">
-                            <div className="group_des text-center">
-                                <img src="assets/img/g3.png" alt=""/>
-                                <h4>Homeschooling gifted children<span>created 4 days ago</span></h4>
-                                <div className="catagory"><a href="/">Public Group</a></div>
-                            </div>
-                        </div>
-                        <div className="col-md-3 col-sm-6 col-xs-12">
-                            <div className="group_des text-center">
-                                <img src="assets/img/g7.png" alt=""/>
-                                <h4>Isabel Forben<span>6 hours ago</span></h4>
-                                <div className="catagory"><a href="/">Public Group</a></div>
-                            </div>
-                        </div>
-                        <div className="col-md-3 col-sm-6 col-xs-12">
-                            <div className="group_des text-center">
-                                <img src="assets/img/g1.png" alt=""/>
-                                <h4>Test group<span>created 2 weeks</span></h4>
-                                <div className="catagory"><a href="/">Public Group</a></div>
-                            </div>
-                        </div>
-                        <div className="col-md-3 col-sm-6 col-xs-12">
-                            <div className="group_des text-center">
-                                <img src="assets/img/g2.png" alt=""/>
-                                <h4>Chuck Henley<span>6 hours ago</span></h4>
-                                <div className="catagory"><a href="/">Public Group</a></div>
-                            </div>
-                        </div>
+                      )
+                    })}
                     </div>
                     <ul className="pagination clearfix">
                         <li><a href="/">1</a></li>
@@ -151,7 +101,7 @@ const Groups = () => {
                         <li><a href="/">4</a></li>
                     </ul>
                 </div>
-            </section>            
+            </section>
         </>
     )
 };
