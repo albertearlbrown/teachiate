ce68223f5d66c91e4452840278e3981856bd39e4
Soufian Elhanafi <soufelhanafi@gmail.com>
2020-11-12 18:40:30 +0100

    add people sections

--- src/pages/People/People.js
+++ src/pages/People/People.js
@@ -1,170 +1,116 @@
-import React from 'react';
-import PageTitle from '../../components/PageTitle'; 
+import React, { useEffect, useState } from 'react';
+import PageTitle from '../../components/PageTitle';
+import Backdrop from '@material-ui/core/Backdrop';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { makeStyles } from '@material-ui/core/styles';
+import axios from 'axios';
+
+const baseUrl = process.env.NODE_ENV === 'development'?"http://localhost:4000":"https://teachiate-backend.fnmotivations.com/"
+
+const useStyles = makeStyles((theme) => ({
+  backdrop: {
+    zIndex: theme.zIndex.drawer + 1,
+    color: '#fff',
+  },
+  text: {
+    top: "50px",
+    position: "relative",
+    left: "-50px"
+  },
+  cover:{
+    width: 73,
+    height: 73,
+    borderRadius: '50%'
+  }
+}));
+
 
 const People = () => {
-    return (
-        <>
+  const classes = useStyles();
+  const [users, setUsers] = useState([])
+  const [totalPages, setTotalPages] = useState(0)
+  const [open, setOpen] = useState(false)
+  const [searchValue, setSearchValue] = useState(null)
+  const [currentPage, setCurrentPage] = useState(0)
 
-            <PageTitle title='Teachiate Members'/>
+  useEffect(()=>{
+    getUsers(1)
+  },[])
 
-            <section className="inner_content people_content">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-9 col-sm-8">
-                            <h3 className="main_sec_title">All Members</h3>
-                        </div>
-                        <div className="col-md-3 col-sm-4">
-                        </div>
+  const getUsers = async (page) => {
+    setOpen(true)
+    axios({
+      url: `${baseUrl}/users/all`,
+      method: 'get',
+      params:{ page, name: searchValue}
+    }).then((response)=>{
+      const { data } = response.data
+      setUsers(data.users)
+      setCurrentPage(data.page)
+      const tt = Math.ceil(data.totalElement / data.limit)
+      setTotalPages(tt)
+      setOpen(false)
+    }).catch((e)=>{
+      setOpen(false)
+      console.log(e);
+    })
+  }
+
+  const getPagination = () => {
+    const list = [];
+    for (let i = 1; i <= totalPages; i++) {
+      list.push(<li className={currentPage === ""+i?"selected":""}><span onClick={()=>getUsers(i)}>{i}</span></li>)
+    }
+    return list;
+  }
+
+  return (
+      <>
+        <Backdrop className={classes.backdrop} open={open} >
+          <CircularProgress color="inherit" />
+          <div className={classes.text}>Loading</div>
+        </Backdrop>
+        <PageTitle title='Teachiate Members'/>
+        <section className="inner_content people_content">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-9 col-sm-8">
+                        <h3 className="main_sec_title">All Members</h3>
                     </div>
-                    <div className="row">
-                        <div className="col-md-12">
-                            <div className="search_flex text-center">
-                                <input type="search" placeholder="Search" className="form-control"/>
-                                <button className="search_btn" type="button">
-                                    <img src="assets/img/search-icon.png" alt=""/>
-                                </button>
-                            </div>
-                        </div>
+                    <div className="col-md-3 col-sm-4">
                     </div>
-                    <div className="row">
-                        <div className="col-md-3 col-sm-6 col-xs-12">
-                            <div className="add_frnd text-center">
-                                <img src="assets/img/m1.png" alt=""/>
-                                <h4>Albert Brown<span>6 hours ago</span></h4>
-                                <div className="catagory">Parent</div>
-                                <a href="#">Add Friend</a>
-                            </div>
-                        </div>
-                        <div className="col-md-3 col-sm-6 col-xs-12">
-                            <div className="add_frnd text-center">
-                                <img src="assets/img/m2.png" alt=""/>
-                                <h4>Al Brown<span>6 hours ago</span></h4>
-                                <div className="catagory">Student</div>
-                                <a href="#">Add Friend</a>
-                            </div>
-                        </div>
-                        <div className="col-md-3 col-sm-6 col-xs-12">
-                            <div className="add_frnd text-center">
-                                <img src="assets/img/m3.png" alt=""/>
-                                <h4>Greg Lee<span>6 hours ago</span></h4>
-                                <div className="catagory">Parent</div>
-                                <a href="#">Add Friend</a>
-                            </div>
-                        </div>
-                        <div className="col-md-3 col-sm-6 col-xs-12">
-                            <div className="add_frnd text-center">
-                                <img src="assets/img/m4.png" alt=""/>
-                                <h4>Nargess Demarco<span>6 hours ago</span></h4>
-                                <div className="catagory">Parent</div>
-                                <a href="#">Add Friend</a>
-                            </div>
-                        </div>
-                        <div className="col-md-3 col-sm-6 col-xs-12">
-                            <div className="add_frnd text-center">
-                                <img src="assets/img/m5.png" alt=""/>
-                                <h4>Jon Smith<span>6 hours ago</span></h4>
-                                <div className="catagory">General Educator</div>
-                                <a href="#">Add Friend</a>
-                            </div>
-                        </div>
-                        <div className="col-md-3 col-sm-6 col-xs-12">
-                            <div className="add_frnd text-center">
-                                <img src="assets/img/m6.png" alt=""/>
-                                <h4>Kamelia Drew<span>6 hours ago</span></h4>
-                                <div className="catagory">Teacher</div>
-                                <a href="#">Add Friend</a>
-                            </div>
-                        </div>
-                        <div className="col-md-3 col-sm-6 col-xs-12">
-                            <div className="add_frnd text-center">
-                                <img src="assets/img/m7.png" alt=""/>
-                                <h4>Kelley Anne Marking<span>6 hours ago</span></h4>
-                                <div className="catagory">General Educator</div>
-                                <a href="#">Add Friend</a>
-                            </div>
-                        </div>
-                        <div className="col-md-3 col-sm-6 col-xs-12">
-                            <div className="add_frnd text-center">
-                                <img src="assets/img/m8.png" alt=""/>
-                                <h4>Albert Brown<span>6 hours ago</span></h4>
-                                <div className="catagory">Student</div>
-                                <a href="#">Add Friend</a>
-                            </div>
-                        </div>
-                        <div className="col-md-3 col-sm-6 col-xs-12">
-                            <div className="add_frnd text-center">
-                                <img src="assets/img/m9.png" alt=""/>
-                                <h4>James Shermann<span>6 hours ago</span></h4>
-                                <div className="catagory">Student</div>
-                                <a href="#">Add Friend</a>
-                            </div>
-                        </div>
-                        <div className="col-md-3 col-sm-6 col-xs-12">
-                            <div className="add_frnd text-center">
-                                <img src="assets/img/m10.png" alt=""/>
-                                <h4>Alexa Jennings<span>6 hours ago</span></h4>
-                                <div className="catagory">Student</div>
-                                <a href="#">Add Friend</a>
-                            </div>
-                        </div>
-                        <div className="col-md-3 col-sm-6 col-xs-12">
-                            <div className="add_frnd text-center">
-                                <img src="assets/img/m11.png" alt=""/>
-                                <h4>Joanne Shuven<span>6 hours ago</span></h4>
-                                <div className="catagory">General Educator</div>
-                                <a href="#">Add Friend</a>
-                            </div>
-                        </div>
-                        <div className="col-md-3 col-sm-6 col-xs-12">
-                            <div className="add_frnd text-center">
-                                <img src="assets/img/m12.png" alt=""/>
-                                <h4>Andy Yang<span>6 hours ago</span></h4>
-                                <div className="catagory">Parent</div>
-                                <a href="#">Add Friend</a>
-                            </div>
-                        </div>
-                        <div className="col-md-3 col-sm-6 col-xs-12">
-                            <div className="add_frnd text-center">
-                                <img src="assets/img/m13.png" alt=""/>
-                                <h4>Katie Knapp<span>6 hours ago</span></h4>
-                                <div className="catagory">Student</div>
-                                <a href="#">Add Friend</a>
-                            </div>
-                        </div>
-                        <div className="col-md-3 col-sm-6 col-xs-12">
-                            <div className="add_frnd text-center">
-                                <img src="assets/img/m14.png" alt=""/>
-                                <h4>Isabel Forben<span>6 hours ago</span></h4>
-                                <div className="catagory">Parent</div>
-                                <a href="#">Add Friend</a>
-                            </div>
-                        </div>
-                        <div className="col-md-3 col-sm-6 col-xs-12">
-                            <div className="add_frnd text-center">
-                                <img src="assets/img/m15.png" alt=""/>
-                                <h4>Antoine Jackson<span>6 hours ago</span></h4>
-                                <div className="catagory">Student</div>
-                                <a href="#">Add Friend</a>
-                            </div>
-                        </div>
-                        <div className="col-md-3 col-sm-6 col-xs-12">
-                            <div className="add_frnd text-center">
-                                <img src="assets/img/m16.png" alt=""/>
-                                <h4>Chuck Henley<span>6 hours ago</span></h4>
-                                <div className="catagory">Student</div>
-                                <a href="#">Add Friend</a>
-                            </div>
-                        </div>
-                    </div>
-                    <ul className="pagination clearfix">
-                        <li><a href="#">1</a></li>
-                        <li><a href="#">2</a></li>
-                        <li><a href="#">3</a></li>
-                        <li><a href="#">4</a></li>
-                    </ul>
                 </div>
-            </section>
-        </>
+                <div className="row">
+                    <div className="col-md-12">
+                        <div className="search_flex text-center">
+                            <input onChange={e=>setSearchValue(e.target.value)} type="search" placeholder="Search" className="form-control"/>
+                            <button onClick={()=>getUsers(1)} className="search_btn" type="button">
+                                <img src="assets/img/search-icon.png" alt=""/>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                  {users.map((user, index) => {
+                    return (
+                      <div key={index} className="col-md-3 col-sm-6 col-xs-12">
+                          <div className="add_frnd text-center">
+                              <img src={user.avatar || "assets/img/m1.png"} alt=""/>
+                              <h4>{user.fullName}</h4>
+                              <div className="catagory">{user.category}</div>
+                              <a href="#">Add Friend</a>
+                          </div>
+                      </div>
+                    )
+                  })}
+
+                </div>
+                <ul className="pagination clearfix">
+                    {getPagination()}
+                </ul>
+            </div>
+        </section>
+      </>
     )
 };
 
