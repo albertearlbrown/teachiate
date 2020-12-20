734a2514836491e5c4a5d4296ee6d7d3339493c2
soufian <soufelhanafi@gmail.com>
2020-12-20 16:53:32 +0100

    add group sent requests

--- src/pages/GroupProfile/invite/index.js
+++ src/pages/GroupProfile/invite/index.js
@@ -1,9 +1,94 @@
 import React, { useState, useEffect } from "react";
+import Moment from 'react-moment'
+import groupActions from '../../../redux/groups/actions'
+import axios from 'axios';
 
 const Invites = ({ group, dispatch, currentUser }) => {
-  const { members } = group;
+  const { members, requests } = group;
   const [membersList, setList] = useState(members);
   const [search, setSearch] = useState("");
+  const [users, setUsers] = useState([])
+  const [totalPages, setTotalPages] = useState(0)
+  const [open, setOpen] = useState(false)
+  const [searchValue, setSearchValue] = useState(null)
+  const [currentPage, setCurrentPage] = useState(0)
+  const [sort, setSort] = useState('date')
+  useEffect(()=>{
+    getUsers(1)
+  },[])
+
+  const getUsers = async (page) => {
+    setOpen(true)
+    axios({
+      url: `/users/all`,
+      method: 'get',
+      params:{ page, sort, name: searchValue}
+    }).then((response)=>{
+      const { data } = response.data
+      setUsers(data.users)
+      setCurrentPage(data.page)
+      const tt = Math.ceil(data.totalElement / data.limit)
+      setTotalPages(tt)
+      setOpen(false)
+    }).catch((e)=>{
+      setOpen(false)
+      console.log(e);
+    })
+  }
+
+  const sendInviation = (userId) => {
+    axios({
+      method: 'post',
+      url: `/group/${group._id}/invitation/send`,
+      data: { userId }
+    }).then(()=>{
+      dispatch({
+        type: groupActions.LOAD_GROUP,
+        payload: {id: group._id }
+      })
+      getUsers(currentPage)
+    }).catch(()=>{
+      console.log("error");
+    })
+  }
+
+  const isMember = (id)=>{
+    if (members.find( m => m.memberId._id === id)) {
+      return null;
+    }else if (requests.find(a=>a.member === id && a.type === 'SENT')) {
+      return(
+        <div className="friend-links">
+          <p className="btn btn-secondary">
+            Cancel Invitation
+          </p>
+        </div>
+      )
+    }else if (requests.find(a=>a.member === id && a.type === 'RECEIVED')) {
+      return (
+        <div className="friend-links">
+          <p className="btn btn-secondary">
+            Accept Invitation
+          </p>
+        </div>
+      );
+    } else {
+      return(
+        <div className="friend-links">
+          <p onClick={()=> sendInviation(id)} className="btn btn-primary">
+            Add To Invite
+          </p>
+        </div>
+      )
+    }
+  }
+
+  const getPagination = () => {
+    const list = [];
+    for (let i = 1; i <= totalPages; i++) {
+      list.push(<li className={currentPage === ""+i?"selected":""}><span onClick={()=>getUsers(i)}>{i}</span></li>)
+    }
+    return list;
+  }
 
   const setSubmit = () => {
     const filter = members.filter((a) => a.memberId.fullName.includes(search));
@@ -45,95 +130,28 @@ const Invites = ({ group, dispatch, currentUser }) => {
         </div>
         <div className="friends_inner_details clearfix">
           {/* friends_inner_details_col */}
-          <div className="friends_inner_details_col clearfix">
-            <div className="friend-info">
-              <div className="friend-pro-image">
-                <img src="/assets/img/friend_request_Avtar_image_1.png" alt="avatar" />
-              </div>
-              <div className="friend-info-details">
-                <a href="#">Jon Smith</a>
-                <div className="group_member_info">
-                  <h5>General Educator</h5>
-                  <h6>12 hours ago</h6>
+          {users.map(user=>(
+            <div key={user._id} className="friends_inner_details_col clearfix">
+              <div className="friend-info">
+                <div className="friend-pro-image">
+                  <img src="/assets/img/friend_request_Avtar_image_1.png" alt="avatar" />
                 </div>
-              </div>
-              <div className="clear" />
-            </div>
-            <div className="friend-links">
-              <a href="#" className="btn btn-secondary">
-                Cancel Invitation
-              </a>
-            </div>
-          </div>
-          {/* friends_inner_details_col */}
-          {/* friends_inner_details_col */}
-          <div className="friends_inner_details_col clearfix">
-            <div className="friend-info">
-              <div className="friend-pro-image">
-                <img src="/assets/img/friend_request_Avtar_image_2.png" alt="avatar" />
-              </div>
-              <div className="friend-info-details">
-                <a href="#">Nargess Demarco</a>
-                <div className="group_member_info">
-                  <h5>Teacher</h5>
-                  <h6>12 hours ago</h6>
+                <div className="friend-info-details">
+                  <a href="#">{user.fullName}</a>
+                  <div className="group_member_info">
+                    <h5>{user.role}</h5>
+                    <h6><Moment fromNow>{user.date}</Moment></h6>
+                  </div>
                 </div>
+                <div className="clear" />
               </div>
-              <div className="clear" />
+              {isMember(user._id)}
             </div>
-            <div className="friend-links">
-              <a href="#" className="btn btn-secondary">
-                Cancel Invitation
-              </a>
-            </div>
-          </div>
-          {/* friends_inner_details_col */}
-          {/* friends_inner_details_col */}
-          <div className="friends_inner_details_col clearfix">
-            <div className="friend-info">
-              <div className="friend-pro-image">
-                <img src="/assets/img/friend_request_Avtar_image_3.png" alt="avatar" />
-              </div>
-              <div className="friend-info-details">
-                <a href="#">Kelley Anne Marking</a>
-                <div className="group_member_info">
-                  <h5>Student</h5>
-                  <h6>12 hours ago</h6>
-                </div>
-              </div>
-              <div className="clear" />
-            </div>
-            <div className="friend-links">
-              <a href="#" className="btn btn-primary">
-                Add To Invite
-              </a>
-            </div>
-          </div>
-          {/* friends_inner_details_col */}
-          {/* friends_inner_details_col */}
-          <div className="friends_inner_details_col clearfix">
-            <div className="friend-info">
-              <div className="friend-pro-image">
-                <img src="/assets/img/friend_request_Avtar_image_4.png" alt="avatar" />
-              </div>
-              <div className="friend-info-details">
-                <a href="#">Katie Knapp</a>
-                <div className="group_member_info">
-                  <h5>Parent</h5>
-                  <h6>12 hours ago</h6>
-                </div>
-              </div>
-              <div className="clear" />
-            </div>
-            <div className="friend-links">
-              <a href="#" className="btn btn-primary">
-                Add To Invite
-              </a>
-            </div>
-          </div>
-          {/* friends_inner_details_col */}
+          ))}
         </div>
-        <div className="pager">Viewing 1 - 4 of 4</div>
+        <ul className="pagination clearfix">
+          {getPagination()}
+        </ul>
       </div>
     </div>
   );
